5396544b35ca9b611e1f677d9e6962cd57b5bcc1
JeromeCourtemanche <jerome.courtemanche2@mail.mcgill.ca>
2024-03-18 19:31:07 -0400

    First commit. Skeleton and starting sim prototype.

---
+++ hole.py
@@ -0,0 +1,8 @@
+class Hole:
+    def __init__(self, holeX, holeY, length, floor):
+        self.holeX = holeX
+        self.holeY = holeY
+        self.length = length
+        self.floor = floor
+        
+        #perhaps this could load canvas from file and return
---
+++ sim.py
@@ -0,0 +1,29 @@
+import time
+import math
+
+class Sim:
+    def __init__(self, ball, hole, angle, speed, spin):#eventually add l/r spin
+        self.ball = ball
+        self.hole = hole
+        self.angle = angle
+        self.speed = speed
+        self.spin = spin
+
+def startSim(self):
+    moving = True
+    lastSimTime = time.time()
+    timeDelta = 0
+
+    while(moving):
+        timeDelta = time.time() - lastSimTime
+        speedX = self.speed*math.cos(self.angle)
+        speedY = self.speed*math.sin(self.angle)
+        self.ball.x += timeDelta*speedX
+        self.ball.y += timeDelta*speedY + 0.5*9.8*(timeDelta**2)
+        #slowdowns and deviations should be taken into account here :)
+
+        if self.ball.y < self.hole.floor:
+            moving = False
+
+        
+
